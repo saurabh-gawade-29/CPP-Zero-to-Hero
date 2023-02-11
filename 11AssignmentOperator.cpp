39f0b03860dcbd5a96cb3767e6d152c568127436
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-02-11 20:29:32 +0530

    Some Add

---
+++ 11AssignmentOperator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int x = 5;
+int y = 6;
+int main()
+{
+    y += 3;
+    // y = y+1
+
+    y -= 3;
+    // y = y-1
+
+    y *= 3;
+    // y = y*1
+
+    y /= 3;
+    // y = y/1
+
+    y %= 3;
+    // y = y%3
+}
--- 1Basic.cpp
+++ 1Basic.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main()
 {
     cout << "Hello World";
-    return 0;
+    return 0; //! WHY Return 0 ??
+    /*
+    return 0: A return 0 means that the program will execute successfully and did what it was intended to do. return 1: A return 1 means that there is some error while executing the program, and it is not performing what it was intended to do.
+    */
 }
 
 // TODO: How to Compile and Run a Code
