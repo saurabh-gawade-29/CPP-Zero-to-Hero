02e1c49f8b50961f69ed195eee42d0a95033f4cc
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-02-11 20:36:11 +0530

    Comparison Operator

--- 12ComparisonOperator.cpp
+++ 12ComparisonOperator.cpp
@@ -5,6 +5,11 @@ int main()
 {
     int x = 5;
     int y = 3;
-    cout << (x > y); // returns 1 (true) because 5 is greater than 3
+    cout << (x > y) << endl;  // returns 1 (true) because 5 is greater than 3
+    cout << (x == y) << endl; // returns 0 (false) because 5 is not equal to 3
+    cout << (x != y) << endl; // returns 1 (true) because 5 is not equal to 3
+    cout << (x < y) << endl;  // returns 0 (false) because 5 is not less than 3
+    cout << (x >= y) << endl; // returns 1 (true) because five is greater than, or equal, to 3
+    cout << (x <= y) << endl; // returns 0 (false) because 5 is neither less than or equal to 3
     return 0;
 }
