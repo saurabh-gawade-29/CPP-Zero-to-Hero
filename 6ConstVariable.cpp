dc8f7077c51310337af8c1b6375d03eaac65336c
Saurabh Gawade <39211222+saurabh-gawade-29@users.noreply.github.com>
2025-01-11 10:41:56 +0530

    updated code

--- 6ConstVariable.cpp
+++ 6ConstVariable.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-const int a = 10;
+const int a = 11;
+int b; // declare
 int main()
 {
-    // a = 11;
+    // a = 15;
+    b = 10; // assignment
+    // cout << a;
+    cout << b; // print
     /*
     TODO: It Will Give You A Error Bcoz a is Constant - Not Mutable
     In function 'int main()':
-    error: assignment of read-only variable 'myNum'
+    error: assignment of read-only variable 'a'
     ? Variable like Pi = 3.14 We Can use Constant
     */
 }
